fadc7c4d00f30282a32827136400d63dd1423445
unknown <nick.dhruwe@gmail.com>
2020-06-04 22:54:10 +0530

    part time

--- EmpWage.java
+++ EmpWage.java
@@ -6,15 +6,19 @@ class EmpWage {
 
    //constants
    int isFullTime=1;
+   int isPartTime=2;
    int empRatePerHour=20;
 
    //variables
    int empHrs=0;
    int empWage=0;
-   double empCheck = Math.floor(Math.random()*10)%2;
+   //computation
+   double empCheck = Math.floor(Math.random()*10)%3;
    if( empCheck == isFullTime){
-      System.out.println("Employee Present");
-   empHrs=8;
+      empHrs=8;
+      }
+   else if (empCheck == isPartTime ){
+      empHrs=4;
       }
    else {
       System.out.println("Employee Absent");
