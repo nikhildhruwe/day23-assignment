b0fb5f598a10fca51b2f10326aef34abd814faf7
unknown <nick.dhruwe@gmail.com>
2020-06-04 23:43:34 +0530

    Limited hours

--- EmpWage.java
+++ EmpWage.java
@@ -1,19 +1,25 @@
-class EmpWage {
+ class EmpWage {
 
    //constants
    public static final int isFullTime=1;
    public static final int isPartTime=2;
    public static final int empRatePerHour=20;
    public static final int totalWorkingDays=20;
+   public static final int maxHrs=100;
+
    public static void main(String[] args) {
 
    System.out.println(" Welcome to Employeee Wage Computation ");
+
    //variables
    int empHrs=0;
    int empWage=0;
    int totalEmpWage=0;
+   int day=0;
+   int totalEmpHrs=0;
    //computation
-   for(int day=0;day<totalWorkingDays;day++) {
+   while(totalEmpHrs<maxHrs && day<totalWorkingDays) {
+      day++;
       int empCheck = (int)Math.floor(Math.random()*10)%3;
       switch ( empCheck){
                case isFullTime : empHrs=8; break;
@@ -21,9 +27,11 @@ class EmpWage {
                default :         empHrs=0; break;
          }
 
-         empWage = empHrs*empRatePerHour;
-         totalEmpWage+=empWage;
+         totalEmpHrs+=empHrs;
       }
+       totalEmpWage=totalEmpHrs*empRatePerHour;
+
       System.out.println("Total Emp Wage = "+totalEmpWage);
    }
 }
+
