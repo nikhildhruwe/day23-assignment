82ccafd590981643e1a98720898083611041b3f0
unknown <nick.dhruwe@gmail.com>
2020-06-05 22:37:53 +0530

    added empWageBuilder class

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,46 +1,32 @@
-public class companyEmpWage{
-
-	private final String company;
-	private final int empRatePerHour;
-	private final int totalWorkingDays;
-	private final int maxHrs;
-	private int totalEmpWage;
-
-
-}
-class EmpWageBuilder {
+public class EmpWageBuilder {
    //constants
    public static final int isFullTime=1;
    public static final int isPartTime=2;
 
 
-	int numOfCompany = 0;
+	private int numOfCompany = 0;
 	private companyEmpWage[] companyEmpWageArray;
 
 	public EmpWageBuilder() {
 
 		companyEmpWageArray =new companyEmpWage[5];
 
-}
+	}
 
   private void addCompanyEmpWage(String company,int empRatePerHour,int totalWorkingDays,int maxHrs) {
 
 
-		compayEmpWageArray[numOfCompany] = new companyEmpWage(company,empRatePerHour,totalWorkingDays,maxHrs);
+		companyEmpWageArray[numOfCompany] = new companyEmpWage(company,empRatePerHour,totalWorkingDays,maxHrs);
 		numOfCompany++;
 	}
 
 	private void computeEmpWage(){
 	for(int i=0;i<numOfCompany;i++) {
-		companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpArray[i]));
-		System.print.out.ln(companyEmpWageArray[i]);
+		companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
+		System.out.println(companyEmpWageArray[i]);
 		}
 }
 
-public void setTotalEmpWage(int totalEmpWage){
-
-		this.totalEmpWage=totalEmpWage;
-}
 
 	public int computeEmpWage(companyEmpWage companyEmpWage){
    //variables
@@ -48,9 +34,8 @@ public void setTotalEmpWage(int totalEmpWage){
    int empWage=0;
    int day=0;
    int totalEmpHrs=0;
-
    //computation
-   while(totalEmpHrs<maxHrs && day<totalWorkingDays) {
+   while(totalEmpHrs<companyEmpWage.maxHrs && day<companyEmpWage.totalWorkingDays) {
       day++;
       int empCheck = (int)Math.floor(Math.random()*10)%3;
       switch ( empCheck){
@@ -61,15 +46,11 @@ public void setTotalEmpWage(int totalEmpWage){
 
          totalEmpHrs+=empHrs;
       }
-       totalEmpWage=totalEmpHrs*empRatePerHour;
 
       System.out.println("Days : " +day+", Emp Hrs: "+empHrs);
+		return totalEmpHrs*companyEmpWage.empRatePerHour;
    }
 
-	public String toString(){
-
-		return ("Total Emp Wage for Company : "+company+" is ="+totalEmpWage);
-}
    public static void main(String[] args) {
 
          System.out.println(" Welcome to Employeee Wage Computation ");
---
+++ companyEmpWage.java
@@ -0,0 +1,28 @@
+public class companyEmpWage{
+
+   public final String company;
+   public final int empRatePerHour;
+   public final int totalWorkingDays;
+   public final int maxHrs;
+   public int totalEmpWage;
+
+
+  public companyEmpWage(String company,int empRatePerHour,int totalWorkingDays,int maxHrs) {
+
+      this.company=company;
+      this.totalWorkingDays=totalWorkingDays;
+      this.maxHrs=maxHrs;
+      this.empRatePerHour=empRatePerHour;
+   }
+
+   public void setTotalEmpWage(int totalEmpWage){
+
+      this.totalEmpWage=totalEmpWage;
+   }
+
+   public String toString(){
+
+      return ("Total Emp Wage for Company : "+company+" is ="+totalEmpWage);
+   }
+
+}
