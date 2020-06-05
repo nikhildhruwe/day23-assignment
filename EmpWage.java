114431a434fbf9b4102ded2dac12991af841e9ae
unknown <nick.dhruwe@gmail.com>
2020-06-05 19:52:53 +0530

    used instance for intitalisation

--- EmpWage.java
+++ EmpWage.java
@@ -3,13 +3,27 @@ class EmpWage {
    public static final int isFullTime=1;
    public static final int isPartTime=2;
 
-   public static void EmpSalary(String company,int empRatePerHour,int totalWorkingDays,int maxHrs) {
+	private final String company;
+	private final int empRatePerHour;
+	private final int totalWorkingDays;
+	private final int maxHrs;
+	private int totalEmpWage;
+
+   public EmpWage(String company,int empRatePerHour,int totalWorkingDays,int maxHrs) {
+
+		this.company=company;
+		this.totalWorkingDays=totalWorkingDays;
+		this.maxHrs=maxHrs;
+		this.empRatePerHour=empRatePerHour;
+	}
+
+	public void computeEmpWage(){
    //variables
    int empHrs=0;
    int empWage=0;
-   int totalEmpWage=0;
    int day=0;
    int totalEmpHrs=0;
+
    //computation
    while(totalEmpHrs<maxHrs && day<totalWorkingDays) {
       day++;
@@ -24,12 +38,21 @@ class EmpWage {
       }
        totalEmpWage=totalEmpHrs*empRatePerHour;
 
-      System.out.println("Total Emp Wage for company : " + company+" = "+totalEmpWage);
+      System.out.println("Days : " +day+", Emp Hrs: "+empHrs);
    }
+
+	public String toString(){
+
+		return ("Total Emp Wage for Company : "+company+" is ="+totalEmpWage);
+}
    public static void main(String[] args) {
 
          System.out.println(" Welcome to Employeee Wage Computation ");
-			EmpSalary("BigBazar",20,2,10);
-			EmpSalary("Ratnadeep",15,2,12);
+			EmpWage bigBazar=new EmpWage("bigBazar",20,5,40);
+			EmpWage dMart=new EmpWage("D-Mart",15,5,30);
+			bigBazar.computeEmpWage();
+			System.out.println(bigBazar);
+			dMart.computeEmpWage();
+			System.out.println(dMart);
       }
 }
